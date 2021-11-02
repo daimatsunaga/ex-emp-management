59f75098bdead8b539189d5434e13037cabd49c8
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-02 16:42:14 +0900

    Employeeリポジトリを作成

---
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -0,0 +1,66 @@
+package jp.co.sample.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Employee;
+
+/**
+ *　従業員用リポジトリ
+ * @author matsunagadai
+ *
+ */
+@Repository
+public class EmployeeRepository {
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = new BeanPropertyRowMapper<>(Employee.class);
+	
+	/**
+	 * idから従業員を検索（１件）
+	 * @param id
+	 * @return　従業員１件分の情報
+	 */
+	public Employee load(Integer id) {
+		String loadSql = "SELECT * FROM employee WHERE id = :id;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		Employee employee = template.queryForObject(loadSql, param, EMPLOYEE_ROW_MAPPER);
+		return employee;
+	}
+	/**
+	 * 全従業員を取得　入社日を降順
+	 * @return　全従業員データ
+	 */
+	public List<Employee> findAll() {
+		String sql = "SELECT * FROM employees ORDER BY hire_date DESC;";
+		List<Employee> employeeList = template.query(sql, EMPLOYEE_ROW_MAPPER);
+		return employeeList;
+	}
+	/**
+	 * idが存在した場合更新、ない場合挿入
+	 * @param employee
+	 */
+	public void update(Employee employee) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
+		
+		if(employee.getId() == null) {
+			String insertSql = "INSERT INTO employees VALUES(:id,:name,:image,:gender,:hireDate,:mailAddress,:zipCode,:address,:telephone,:salary,:characteristic,:dependentsCount);";
+			template.update(insertSql, param);
+			System.out.println("追加されました");
+		} else {
+			String updateSql = "UPDATE employees SET name = :name, image = :image, hire_date = :hireDate, mail_address = :mailAddress, zip_code = :zipCode, telephone = :telephone, salary = :salary, characteristic = :characteristic, dependentsCount = :dependentsCount;";
+			template.update(updateSql, param);
+			System.out.println("更新されました");
+		}
+	}
+}
