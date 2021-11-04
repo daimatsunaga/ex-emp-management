c796494b316f68d93473986cb4fe9a5e4ce96989
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-04 10:57:07 +0900

    AdmnistratorControllerにtoLoginメソッドの追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 @Controller
@@ -22,6 +23,11 @@ public class AdministratorController {
 		return new InsertAdministratorForm();
 	}
 	
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		return new LoginForm();
+	}
+	
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
@@ -33,6 +39,11 @@ public class AdministratorController {
 		BeanUtils.copyProperties(form, administrator);
 		administratorService.insert(administrator);
 		return "redirect:/";
+	}
+	
+	@RequestMapping("/")
+	public String toLogin() {
+		return "administrator/login";
 	}
 
 }
