4e91535fdddb01517715479c79ef5d8c6149399a
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-04 12:05:00 +0900

    AdministratorControllerにloginメソッドの追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,6 +20,9 @@ public class AdministratorController {
 	
 	@Autowired
 	private AdministratorService administratorService;
+	
+	@Autowired
+	private HttpSession session;
 	
 	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
@@ -40,10 +46,28 @@ public class AdministratorController {
 		administratorService.insert(administrator);
 		return "redirect:/";
 	}
-	
+	/**
+	 * ログイン画面に遷移
+	 * @return
+	 */
 	@RequestMapping("/")
 	public String toLogin() {
 		return "administrator/login";
 	}
+	
+	@RequestMapping("/login")
+	public String login(LoginForm form, Model model) {
+		Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+		if(administrator == null) {
+			model.addAttribute("errors", "メールアドレスまたはパスワードが不正です。");
+			return toLogin();
+		} else {
+			session.setAttribute("administratorName", administrator.getName());
+		}
+		
+		return "forward:employee/showList";
+		
+	}
+	
 
 }
