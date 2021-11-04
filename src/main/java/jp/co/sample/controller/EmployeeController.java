3b4a2b3d290919fc6e274cf3097d9d349eecf9f2
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-04 14:40:51 +0900

    EmployeeControllerにshowDetailメソッドを定義

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,11 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
-
+/**
+ * 従業員情報操作のためのコントローラー
+ * @author matsunagadai
+ *
+ */
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
@@ -17,10 +23,27 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 	
+	@ModelAttribute
+	public UpdateEmployeeForm setUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
+	/**
+	 * 全従業員情報をrequestスコープに格納し、リスト一覧へ遷移。
+	 * @param model
+	 * @return 全従業員情報
+	 */
 	@RequestMapping("/showList")
 	public String showList(Model model) {
 		List<Employee> employeeList = employeeService.showLits();
 		model.addAttribute("employeeList", employeeList);
 		return "employee/list";
+	}
+	
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee", employee);
+		return "employee/detail";
 	}
 }
