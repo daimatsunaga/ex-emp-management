d03ba42c993a5347437d28152ea8c697ee0ed216
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-05 10:38:50 +0900

    管理者登録にバリデーションの設定

--- src/main/java/jp/co/sample/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -1,12 +1,21 @@
 package jp.co.sample.form;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * 管理者用のフォームクラス
  * @author matsunagadai
  *
  */
 public class InsertAdministratorForm {
+	@NotBlank(message="氏名を入力してください")
 	private String name;
+	@NotBlank(message="メールアドレスを入力してください")
+	@Email(message="正しいメールアドレスを入力してください")
 	private String mailAddress;
+	@Size(min=1, max=127, message="パスワードを入力してください")
 	private String password;
 	
 	//ゲッターセッター
